ab9a37d5b633dc3fe4f7f7130c6043eee23969c3
Ivan Lev <fanatos@mail.ru>
2024-08-25 00:34:41 +0500

    feat: add joi validators

---
+++ src/middlewares/exhibitValidator.js
@@ -0,0 +1,40 @@
+const { celebrate, Joi } = require("celebrate");
+
+const { URL_REGEXP } = require("../constants");
+
+module.exports.exhibitValidator = celebrate({
+  body: Joi.object().keys({
+    id: Joi.number().min(0).max(9999).required(),
+    name: Joi.string().min(10).required(),
+    age: Joi.string().min(4),
+    category: Joi.string().hex().length(24).required(),
+    thumbnail: Joi.string(),
+    style: Joi.string().required(),
+    description: Joi.string().required(),
+    potterName: Joi.string(),
+    potterJapaneseName: Joi.string(),
+    potterLifeDates: Joi.string().min(5),
+    potterPhoto: Joi.string(),
+    potterInfo: Joi.string(),
+    additionalDescription: Joi.string(),
+    additionalPhotos: Joi.boolean(),
+    additionalPhotosCount: Joi.number(),
+    price: Joi.number(),
+    weight: Joi.number(),
+    height: Joi.number(),
+    length: Joi.number(),
+    width: Joi.number(),
+    diameter: Joi.number(),
+    footDiameter: Joi.number(),
+    volume: Joi.number(),
+    weightOfSet: Joi.number(),
+    complectation: Joi.string().required(),
+    preservation: Joi.string().required(),
+  }),
+});
+
+module.exports.exhibitIdValidator = celebrate({
+  params: Joi.object().keys({
+    id: Joi.number().min(0).max(9999).required(),
+  }),
+});
---
+++ src/middlewares/exhibitionValidator.js
@@ -0,0 +1,22 @@
+const { celebrate, Joi } = require("celebrate");
+
+const { URL_REGEXP } = require("../constants");
+
+module.exports.exhibitionValidator = celebrate({
+  body: Joi.object().keys({
+    id: Joi.number().greater(0).required(),
+    year: Joi.number().greater(2020).required(),
+    dates: Joi.string().min(5).required(),
+    city: Joi.string().required(),
+    address: Joi.string().required(),
+    place: Joi.string().required().required(),
+    name: Joi.string().required().required(),
+    link: Joi.string().allow("").pattern(URL_REGEXP),
+    description: Joi.string().allow("").required(),
+    photosCount: Joi.number().min(0).required(),
+    poster: Joi.boolean().required(),
+    curators: Joi.string().allow(""),
+    organisators: Joi.string().allow(""),
+    isActive: Joi.boolean().required(),
+  }),
+});
