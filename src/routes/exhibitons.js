27042590c6f11573323fd78e9473b63fe79ab112
Ivan Lev <fanatos@mail.ru>
2024-08-26 00:17:09 +0500

    feat: add exhibition related routes

--- src/routes/exhibitons.js
+++ src/routes/exhibitons.js
@@ -3,11 +3,18 @@ const router = require("express").Router();
 const {
   getExhibitions,
   createExhibition,
+  updateExhibition,
+  deleteExhibition,
 } = require("../controllers/exhibitions");
 
-const { exhibitionValidator } = require("../middlewares/exhibitionValidator");
+const {
+  exhibitionValidator,
+  exhibitionIdValidator,
+} = require("../middlewares/exhibitionValidator");
 
 router.get("/", getExhibitions);
 router.post("/", exhibitionValidator, createExhibition);
+router.patch("/:id", exhibitionValidator, updateExhibition);
+router.delete("/:id", exhibitionIdValidator, deleteExhibition);
 
 module.exports = router;
