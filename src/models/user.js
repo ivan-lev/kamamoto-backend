595fa9292ec01986bde3b9070e96e061302fbe18
Ivan Lev <fanatos@mail.ru>
2024-08-27 21:32:47 +0500

    feat: add user schema and mongoose model

---
+++ src/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const { AuthorizationError } = require("../errors");
+const { ERROR_MESSAGES } = require("../constants");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Поле email должно быть заполнено"],
+      unique: [true, "Этот адрес почты уже используется"],
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: "Некорректный email",
+      },
+    },
+
+    password: {
+      type: String,
+      required: [true, "Поле password должно быть заполнено"],
+      select: false,
+    },
+  },
+  { versionKey: false }
+);
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        throw new AuthorizationError(ERROR_MESSAGES.USER_WRONG_CREDENTIALS);
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          throw new AuthorizationError(ERROR_MESSAGES.USER_WRONG_CREDENTIALS);
+        }
+        return user;
+      });
+    });
+};
+
+module.exports = mongoose.model("user", userSchema);
