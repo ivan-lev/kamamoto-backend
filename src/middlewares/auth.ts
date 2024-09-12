be84559e970af2fddce553daddf1f51ba90020b7
Ivan Lev <fanatos@mail.ru>
2024-09-12 23:53:26 +0500

    feat/refactor: rewrite auth function using TS and ES6

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,14 +1,14 @@
-const jwt = require("jsonwebtoken");
+import { JWT_SECRET, NODE_ENV } from "../config";
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
 const { AuthorizationError } = require("../errors");
 const { ERROR_MESSAGES } = require("../constants");
 
-const { JWT_SECRET, NODE_ENV } = process.env;
-
-module.exports = (req, res, next) => {
+export const auth = (req: any, res: Response, next: NextFunction) => {
   const { authorization } = req.headers;
 
-  // pass next all get requests of it is not user login checking
+  // pass next all get requests if it is not user login checking
   if (req.method === "GET" && req.originalUrl !== "/users/") {
     return next();
   }
