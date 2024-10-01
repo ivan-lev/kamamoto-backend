00abfc041822915ffd823c089653e9a96b359437
Ivan Lev <fanatos@mail.ru>
2024-10-01 21:04:33 +0500

    refactor: reqrite app.ts in TS and ES6 style

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,38 @@
-import dotenv from "dotenv";
-dotenv.config();
+import dotenv from 'dotenv'
 
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const helmet = require("helmet");
-const { errors } = require("celebrate");
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import helmet from 'helmet'
 
-const { limiter } = require("./middlewares/limiter");
-const { requestLogger, errorLogger } = require("./middlewares/logger");
-import { errorHandler } from "./middlewares/error-handler";
-const { routes } = require("./routes");
+import { limiter } from './middlewares/limiter'
+import { requestLogger, errorLogger } from './middlewares/logger'
+import { errors } from 'celebrate'
+import { errorHandler } from './middlewares/error-handler'
+import routes from './routes'
 
-import { PORT, DB_URL } from "./config";
+dotenv.config()
 
-const app = express();
+const PORT = process.env.PORT ?? '3001'
+const DB_URL: string = process.env.DB_URL ?? 'mongodb://127.0.0.1:27017/kamamotodb'
+const app = express()
 
-mongoose.connect(DB_URL).catch((error: any) => console.log(error));
+mongoose.connect(DB_URL).catch((error: any) => { console.log(error) })
 
-app.use(limiter); // limit requests count
-app.use(cors()); // cross-domain settings
-app.use(requestLogger); // winston requests logger
-app.use(helmet()); // protect headers
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(routes); // all routes goes through here
+app.use(limiter) // limit requests count
+app.use(cors()) // cross-domain settings
+app.use(requestLogger) // winston requests logger
+app.use(helmet()) // protect headers
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(routes) // all routes goes through here
 
-app.use(errorLogger); // winston error logger
-app.use(errors()); // celebrate error handler
-app.use(errorHandler); // final error handler
+app.use(errorLogger) // winston error logger
+app.use(errors()) // celebrate error handler
+app.use(errorHandler) // final error handler
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log(`DB path is ${DB_URL}`);
-});
+  console.log(`App listening on port ${PORT}`)
+  console.log(`DB path is ${DB_URL}`)
+})
