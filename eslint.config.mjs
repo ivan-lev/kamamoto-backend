21057c7df0b22031e9fe008f852c704bfb3a5519
Ivan Lev <fanatos@mail.ru>
2024-10-12 20:45:48 +0500

    feat: update packages and settings

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu({
+	stylistic: {
+		indent: 2,
+		quotes: 'single',
+		semi: true,
+	},
+
+	rules: {
+		'eslint-comments/no-unlimited-disable': 'off',
+		'no-underscore-dangle': ['error', { allow: ['_id'] }],
+	},
+
+	// TypeScript and Vue are auto-detected, you can also explicitly enable them:
+	typescript: true,
+	vue: true,
+	jsonc: false,
+	yaml: false,
+	jsx: true,
+	tsx: true,
+	node: true,
+
+	// `.eslintignore` is no longer supported in Flat config, use `ignores` instead
+	ignores: [],
+});
